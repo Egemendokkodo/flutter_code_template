fc86ca66ad85bd22e64659d5aff8a0ac147e5dc2
EgemenSevgi <egemensevgi40@gmail.com>
2025-12-16 23:07:53 +0300

    removed unnecesarry prints

--- lib/core/network/interceptors.dart
+++ lib/core/network/interceptors.dart
@@ -6,38 +6,19 @@ class ApiInterceptor extends Interceptor {
   ApiInterceptor({this.token});
 
   @override
-  void onRequest(
-    RequestOptions options,
-    RequestInterceptorHandler handler,
-  ) {
-    options.headers.addAll({
-      'Accept': 'application/json',
-      if (token != null) 'Authorization': 'Bearer $token',
-    });
-
-    // Debug log
-    print('➡️ ${options.method} ${options.path}');
-    print('Headers: ${options.headers}');
-    print('Body: ${options.data}');
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    options.headers.addAll({'Accept': 'application/json', if (token != null) 'Authorization': 'Bearer $token'});
 
     super.onRequest(options, handler);
   }
 
   @override
-  void onResponse(
-    Response response,
-    ResponseInterceptorHandler handler,
-  ) {
-    print('✅ ${response.statusCode} ${response.data}');
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
     super.onResponse(response, handler);
   }
 
   @override
-  void onError(
-    DioException err,
-    ErrorInterceptorHandler handler,
-  ) {
-    print('❌ ${err.response?.statusCode} ${err.message}');
+  void onError(DioException err, ErrorInterceptorHandler handler) {
     super.onError(err, handler);
   }
 }
